d5502eadd77153d1814f5c8fcde15deb16038549
Brian Yee <bgyee@ucalgary.ca>
2016-03-20 12:45:20 -0600

    fixed test

--- qtProject/revsurface.cpp
+++ qtProject/revsurface.cpp
@@ -24,26 +24,19 @@ QVector3D *RevSurface::eval(float u, float v)
 
 vector<QVector3D*> *RevSurface::evalQuads(float u_step, float v_step)
 {
-/*
-    for (int y = 0; v < ONE_REV_DEG; v += v_step)
-    {
-        for (int u = 0; u < ONE_REV_DEG; u += u_step)
-        {
-            float u_rad = u * ONE_REV_DEG;
-            float v_rad = v * ONE_REV_DEG;
-
-            QVector2D *p0 = eval(u, v);
-        }
-    }
-*/
     vector<QVector3D*> *result = new vector<QVector3D*>;
 
     float V_INCREMENT = v_step * M_PI * 2;
+    int num_u = ceil(1.0 / u_step);
+    int num_v = ceil(1.0 / v_step);
 
-    for (float u = 0; u <= 1 - u_step; u += u_step)
+    for (int i_u = 0; i_u < num_u; i_u++)
     {
-        for (float v = 0; v < M_PI * 2; v+= V_INCREMENT)
+        for (int i_v = 0; i_v < num_v; i_v++)
         {
+            float u = u_step * i_u;
+            float v = v_step * i_v * M_PI * 2;
+
             QVector3D **quad = new QVector3D*[4];
 
             QVector2D *u_pt1 = curve->evalPoint(u);
@@ -64,11 +57,12 @@ vector<QVector3D*> *RevSurface::evalQuads(float u_step, float v_step)
             quad[3] = new QVector3D((*u_pt2)[0]  * cos(v), (*u_pt2)[1], (*u_pt2)[0] * sin(v));
 
             for (int i = 0; i < 4; i++)
+            {
                 result->push_back(quad[i]);
+                //qDebug() << i_v + (i_u * num_u) << ": " << *(quad[i]);
+            }
         }
     }
-    /*QMatrix4x4 r;
-    r.rotate(rad, QVector3D(0,1,0));*/
 
     return (result);
 }
--- qtProject/tests.cpp
+++ qtProject/tests.cpp
@@ -30,7 +30,7 @@ void RevSurface_1()
 }
 
 // tesselation test
-void RevSurface_Quad1()
+void RevSurface_Quads1()
 {
     vector<QVector2D*> pts;
     pts.push_back(new QVector2D(1, 0));
@@ -52,9 +52,9 @@ void RevSurface_Quad1()
     vector<QVector3D*> *actual = rs.evalQuads(ustep, vstep);
 
     bool pass = true;
-    for (int i = 0 ; i < actual->size(); i++)
+    for (int i = 0 ; i < expected.size(); i++)
     {
-        QVector3D &a = *(*actual)[i];
+        QVector3D &a = *(expected)[i];
         QVector3D &b = *(*actual)[i];
         for (int j = 0 ; j < 3; j++)
         {
@@ -69,8 +69,71 @@ void RevSurface_Quad1()
         qDebug() << "RevSurface_Quad1 pass\n";
 }
 
+
+// tesselation test
+void RevSurface_Quads2()
+{
+    vector<QVector2D*> pts;
+    pts.push_back(new QVector2D(1, 0));
+    pts.push_back(new QVector2D(1, 1));
+    pts.push_back(new QVector2D(1, 2));
+
+    BSpline c = BSpline(2, &pts);  // m, pts, order, //..  standard knots
+
+    RevSurface rs = RevSurface(&c);
+    float ustep = 1;
+    float vstep = 0.25;
+
+    vector<QVector3D*> expected;
+    QVector3D p1 = QVector3D(1, 0, 0);
+    QVector3D p2 = QVector3D(0, 0, 1);
+    QVector3D p3 = QVector3D(0, 2, 1);
+    QVector3D p4 = QVector3D(1, 2, 0);
+
+    QMatrix4x4 r;
+    r.rotate(-vstep * 360, 0, 1, 0);
+
+    qDebug() << r;
+    for (int i = 0 ; i < 4; i++)
+    {
+        expected.push_back(new QVector3D(p1));
+        expected.push_back(new QVector3D(p2));
+        expected.push_back(new QVector3D(p3));
+        expected.push_back(new QVector3D(p4));
+/*
+        qDebug() << i << ": " << p1;
+        qDebug() << i << ": " << p2;
+        qDebug() << i << ": " << p3;
+        qDebug() << i << ": " << p4;*/
+
+        p1 = r * p1;
+        p2 = r * p2;
+        p3 = r * p3;
+        p4 = r * p4;
+    }
+    vector<QVector3D*> *actual = rs.evalQuads(ustep, vstep);
+
+    bool pass = true;
+    for (int i = 0 ; i < expected.size(); i++)
+    {
+        QVector3D &a = *(expected[i]);
+        QVector3D &b = *(*actual)[i];
+        for (int j = 0 ; j < 3; j++)
+        {
+            if (abs(a[j] - b[j]) > 0.01)
+               pass = false;
+        }
+    }
+
+    if (!pass)
+        qDebug() << "RevSurface_Quad2 fail\n";
+    else
+        qDebug() << "RevSurface_Quad2 pass\n";
+}
+
 Tests::Tests()
 {
     RevSurface_1();
-    RevSurface_Quad1();
+    //RevSurface_Quads1();
+    RevSurface_Quads2();
 }
